11b96f22e940ed261b6dbe0e8a3324a534c7f6dd
spencer burke <info.spencer.burke@gmail.com>
2026-01-28 15:38:14 -0800

    Add more tutorial code for the product emails

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get "subscribers/create"
   resource :session
   resources :passwords, param: :token
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "products#index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
 end
