0c9741452b9a7427520dc7fecde658b5db005b6e
spencer burke <info.spencer.burke@gmail.com>
2026-01-29 14:59:50 -0800

    Add first and last name to use model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_28_222105) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_29_224758) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -76,6 +76,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_28_222105) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
