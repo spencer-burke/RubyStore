e15bee95704fbe54740dab11ebf4e9eee99e84d7
spencer burke <info.spencer.burke@gmail.com>
2026-01-29 17:54:35 -0800

    Fix form with proper fields

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -2,4 +2,19 @@ class SignUpsController < ApplicationController
   def show
     @user = User.new
   end
+
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def sign_up_params
+      params.expect(user: [ :first_name, :last_name, :email_address, :password, :password_confirmation ])
+    end
 end
