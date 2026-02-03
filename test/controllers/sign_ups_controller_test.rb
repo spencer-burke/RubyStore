bd0c142f8e7909ac796f377685b034e9c21e7724
spencer burke <info.spencer.burke@gmail.com>
2026-02-03 11:43:23 -0800

    Add auth tests

--- test/controllers/sign_ups_controller_test.rb
+++ test/controllers/sign_ups_controller_test.rb
@@ -1,8 +1,29 @@
 require "test_helper"
 
 class SignUpsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get sign_ups_show_url
+  test "view sign up" do
+    get sign_up_path
     assert_response :success
+  end
+
+  test "view sign up when authenticated" do
+    sign_in_as users(:one)
+    get sign_up_path
+    assert_redirected_to root_path
+  end
+
+  test "successful sign up" do
+    assert_difference "User.count" do
+      post sign_up_path, params: { user: { first_name: "Example", last_name: "User", email_address: "example@user.org", password: "password", password_confirmation: "password" } }
+      assert_redirected_to root_path
+    end
+  end
+
+  test "sign up ignores admin attribute" do
+    assert_difference "User.count" do
+      post sign_up_path, params: { user: { first_name: "Example", last_name: "User", email_address: "example@user.org", password: "password", password_confirmation: "password", admin: true } }
+      assert_redirected_to root_path
+    end
+    refute User.find_by(email_address: "example@user.org").admin?
   end
 end
